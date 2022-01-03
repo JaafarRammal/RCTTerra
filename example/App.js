9297a2ecbf1bf30f0d6475fd179f0f50e06d1da8
JaafarRammal <jarammal@gmail.com>
2022-01-03 17:56:24 +0000

    bundled all together

---
+++ example/App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  useColorScheme,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import TerraApple from 'react-native-terra-apple';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  TerraApple.initApple("devid", "apikey", userID => {
+    console.log(userID);
+    TerraApple.getAthlete();
+    TerraApple.getBody(new Date("Jan 02 2022").toISOString(), new Date().toISOString());
+    TerraApple.getDaily(new Date("Jan 02 2022").toISOString(), new Date().toISOString());
+    TerraApple.getSleep(new Date("Jan 02 2022").toISOString(), new Date().toISOString());
+    TerraApple.getActivity(new Date("Jan 02 2022").toISOString(), new Date().toISOString());
+  });
+
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ index.d.ts
@@ -0,0 +1,57 @@
+declare module "react-native-terra-apple-test" {
+  export interface RNTerra {
+    /**
+     * Authenticate user for linking Apple HealthKit to Terra.
+     * @param devId Terra devID
+     * @param apiKey Terra apiKey
+     * @param callback function called with userID upon completed authentication
+     */
+    initApple(
+      devId: string,
+      apiKey: string,
+      callback: (userID: string) => void
+    ): void;
+
+    /**
+     * Send Terra Athlete data to Dev webhook
+     */
+    getAthlete(): void;
+
+    /**
+     * Send Terra Body data to Dev webhook
+     * @param startDate start date in ISOString format
+     * @param endDate end date in ISOString format
+     */
+    getBody(startDate: string, endDate: string): void;
+
+    /**
+     * Send Terra Daily data to Dev webhook
+     * @param startDate start date in ISOString format
+     * @param endDate end date in ISOString format
+     */
+    getDaily(startDate: string, endDate: string): void;
+
+    /**
+     * Send Terra Activity data to Dev webhook
+     * @param startDate start date in ISOString format
+     * @param endDate end date in ISOString format
+     */
+    getActivity(startDate: string, endDate: string): void;
+
+    /**
+     * Send Terra Sleep data to Dev webhook
+     * @param startDate start date in ISOString format
+     * @param endDate end date in ISOString format
+     */
+    getSleep(startDate: string, endDate: string): void;
+
+    /**
+     * Deauth user from Terra service
+     */
+    deauth(): void;
+  }
+
+  const TerraApple: RNTerra;
+
+  export default TerraApple;
+}
