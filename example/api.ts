42647b9d77b20388994b6634d3ea4d32c68d88c2
JaafarRammal <jarammal@gmail.com>
2022-01-31 18:28:53 +0000

    [1.0.8] Update example

--- example/api.ts
+++ example/api.ts
@@ -6,12 +6,12 @@ const apiKey = 'apiKey';
 export function FirstConnection() {
   TerraApple.auth(devID, apiKey, 'refID')
     .then(res => {
-      ConnectUser(res.userID, true);
+      ConnectUser(true);
       console.log(res);
     })
     .catch(e => console.log(e.message, e.code));
 }
 
-export function ConnectUser(userID: string, autoFetch: boolean) {
-  TerraApple.initTerra(devID, apiKey, userID, autoFetch);
+export function ConnectUser(autoFetch: boolean) {
+  TerraApple.initTerra(devID, apiKey, autoFetch);
 }
